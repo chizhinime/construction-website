8095a180ab6c4284abcd28b4db2ebbff66ed9812
CHIZHINIME CHIJIOKE <131488218+chizhinime@users.noreply.github.com>
2024-08-11 18:42:45 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -51,3 +51,15 @@ document.addEventListener('DOMContentLoaded', () => {
         navLinks.classList.toggle('active');
     });
 });
+
+<!--Start of Tawk.to Script-->
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/668a688beaf3bd8d4d1911ae/1i26agote';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+<!--End of Tawk.to Script-->
